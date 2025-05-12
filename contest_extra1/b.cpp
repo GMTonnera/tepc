aa00460fac5b3dba7ca1cf43c83c6b50c157e18c
Gustavo Mello Tonnera <gmtonnera@gmail.com>
2025-05-12 16:20:56 -0300

    contest_extra1: b

--- contest_extra1/b.cpp
+++ contest_extra1/b.cpp
@@ -2,22 +2,35 @@
 
 using namespace std;
 
+
+
 int temp(int n, int m) {
+    vector<int> visited(max(n,m)*2+1, 0);
     vector<int> v;
     queue<vector<int>> q;
+    // int temp;
 
     q.push({n,m,0});
     while(true) {
         v = q.front();
         q.pop();
+        visited[v[0]] = 1;
+        // cout << v[0] << " " << v[1] << " " << v[2] << endl;
+        // for(int i = 0; i < visited.size(); i++)
+        //     cout << i << ": " << visited[i] << endl;
+        // cout << endl;
         if(v[0] == v[1]) {
             return v[2];
         }
-        if (v[0]*2 < v[1])
+        if (v[0] < v[1] and visited[v[0]*2] == 0) {
             q.push({v[0]*2, v[1], v[2]+1});
-        // if (v[0] > v[1])
-        if (v[0] > 0)
+        }
+            
+        
+        if (v[0] > 0 and visited[v[0]-1] == 0) {
             q.push({v[0]-1, v[1], v[2]+1});
+        }
+            
     }
 }
 
